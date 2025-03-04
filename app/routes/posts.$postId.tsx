78fecfa7d227facd96515432b9ac067bb008cef8
Gregory Collett <gregcollett@mixcloud.com>
2025-03-04 16:35:48 +0000

    head is not called when you hit an instance of not found route

---
+++ app/routes/posts.$postId.tsx
@@ -0,0 +1,42 @@
+import { Link, createFileRoute } from '@tanstack/react-router'
+import { fetchPost } from '../utils/posts'
+import { NotFound } from '~/components/NotFound'
+import { PostErrorComponent } from '~/components/PostError'
+import { seo } from '~/utils/seo'
+
+export const Route = createFileRoute('/posts/$postId')({
+  loader: ({ params: { postId } }) => fetchPost({ data: postId }),
+  errorComponent: PostErrorComponent,
+  component: PostComponent,
+  notFoundComponent: () => {
+    return <NotFound>Post not found</NotFound>
+  },
+  head: ({ loaderData }) => console.log('this is not called when this is a not found route but it was previously', loaderData) || ({
+    meta: [
+      ...seo({
+        title: loaderData.title,
+      }),
+    ],
+  })
+})
+
+function PostComponent() {
+  const post = Route.useLoaderData()
+
+  return (
+    <div className="space-y-2">
+      <h4 className="text-xl font-bold underline">{post.title}</h4>
+      <div className="text-sm">{post.body}</div>
+      <Link
+        to="/posts/$postId/deep"
+        params={{
+          postId: post.id,
+        }}
+        activeProps={{ className: 'text-black font-bold' }}
+        className="block py-1 text-blue-800 hover:text-blue-600"
+      >
+        Deep View
+      </Link>
+    </div>
+  )
+}
